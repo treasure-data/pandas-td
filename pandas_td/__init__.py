108970dc382065f23e467f16eab09cb2f0d21d8d
Keisuke Nishida <knishida@treasure-data.com>
2015-05-27 03:18:51 +0900

    Define __all__

--- pandas_td/__init__.py
+++ pandas_td/__init__.py
@@ -1,5 +1,11 @@
-from .td import Connection
 from .td import connect
 from .td import read_td
 from .td import read_td_query
 from .td import to_td
+
+__all__ = [
+    'connect',
+    'read_td',
+    'read_td_query',
+    'to_td',
+]
