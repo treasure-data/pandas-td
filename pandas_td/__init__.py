fc643469626e1b3851346e9b81f5bd2ae7156973
Keisuke Nishida <knishida@treasure-data.com>
2015-05-30 21:26:23 +0900

    Add read_td_table.

--- pandas_td/__init__.py
+++ pandas_td/__init__.py
@@ -1,11 +1,13 @@
 from .td import connect
 from .td import read_td
 from .td import read_td_query
+from .td import read_td_table
 from .td import to_td
 
 __all__ = [
     'connect',
     'read_td',
     'read_td_query',
+    'read_td_table',
     'to_td',
 ]
