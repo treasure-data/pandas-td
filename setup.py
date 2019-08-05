5f01f3d713c1d8f7f84aeac4136100ad3d02be49
Atsushi Odagiri <aodagx@gmail.com>
2019-08-05 11:47:46 +0900

    added dependency to isort

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(
     url="https://github.com/treasure-data/pandas-td",
     install_requires=requires,
     extras_require={
-        "dev": ["pytest>=3.6", "pytest-cov", "black==19.3b0"],
+        "dev": ["pytest>=3.6", "pytest-cov", "black==19.3b0", "isort"],
     },
     packages=find_packages(),
     license="Apache License 2.0",
